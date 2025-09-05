44ec2c70449824573180fba03f3a83b9d3f246c4
Your Name <you@example.com>
2025-09-05 14:54:43 +0800

    Add header component and update styles

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/partials/header/header";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
